3b782e7c8ad2be50c690385263261f32db537a6b
Prana Apsara Wijaya <1911510996@student.budiluhur.ac.id>
2022-03-08 21:26:51 +0700

    first init

---
+++ BinarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BinarApp
+//
+//  Created by Prana Apsara Wijaya on 08/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
