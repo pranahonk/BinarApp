df7a5d6765022b82dc73f393fd4875562e849abb
Prana Apsara Wijaya <1911510996@student.budiluhur.ac.id>
2022-03-10 00:19:01 +0700

    make button

--- BinarApp/ViewController.swift
+++ BinarApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var blueButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(clickedRepeatOnPlate))
+        tapGesture.numberOfTapsRequired = 2
+        blueButton.addGestureRecognizer(tapGesture)
+        setUpBlueButton()
+    }
+    
+    @IBAction func onBlueButtonTouchUpInside(_ sender: UIButton, forEvent event: UIEvent) {
+        blueButton.setTitle("blue is still enabled", for: .normal)
+    }
+    
+    func addDoubleTapEvent() {
+       let tapGesture = UITapGestureRecognizer(target: self, action: #selector(clickedRepeatOnPlate(gesture:)))
+       tapGesture.numberOfTapsRequired = 2
+        blueButton.addGestureRecognizer(tapGesture)
+     }
+
+     @objc func clickedRepeatOnPlate(gesture: UITapGestureRecognizer) {
+         guard let button = gesture.view as? UIButton else { return }
+         blueButton.isEnabled.toggle()
+         blueButton.setTitle("blue disabled", for: .disabled)
+     }
+    
+    func setUpBlueButton(){
+        blueButton.setTitle("blue enabled", for: .normal)
+        blueButton.backgroundColor = .blue
+        blueButton.setTitleColor(.white, for: .normal)
     }
 
-
+    @IBAction func onBlueButtonTouchDownRepeat(_ sender: UIButton) {
+    }
+    
 }
 
+class customButton: UIButton {
+    
+}
